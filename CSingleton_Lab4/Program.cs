bf7c75233618b72644df8b5094d408ee7cc959bb
Christopher <cjsing11@gmail.com>
2018-07-19 16:13:26 -0400

    added notes, header precision, & modified alignment

--- CSingleton_Lab4/Program.cs
+++ CSingleton_Lab4/Program.cs
@@ -10,40 +10,40 @@ namespace CSingleton_Lab4
     {
         static void Main(string[] args)
         {
+            //declaring while loop default and header elements
             string input = "y";
             string numberw = "Number";
             string squaredw = "Squared";
             string cubedw = "Cubed";
-            string sevenmarks = "=======";
+            string sevenmks = "=======";
+            string sixmks = "======";
 
 
-            while (input.ToLower() == "y")
+            while (input.ToLower() == "y")//for exit prompt
             { 
 
-                Console.WriteLine("Please enter an integer:");
+                Console.WriteLine("Please enter an integer:");//salutation
 
-                int n = int.Parse(Console.ReadLine());
+                int n = int.Parse(Console.ReadLine());//user input
 
-                string header = string.Format("{0, 10}  {1, 10}  {2, 10}", numberw, squaredw, cubedw);
-                Console.WriteLine(header);
-                string border = string.Format("{0, 10}  {1, 10}  {2, 10}", sevenmarks, sevenmarks, sevenmarks);
-                Console.WriteLine(border);
+                string header = string.Format("{0, -10}  {1, -10}  {2, -10}", numberw, squaredw, cubedw);
+                Console.WriteLine(header);//create row of labels for "table".
 
-                for (int i = 1; i <= n; i++)
+                string border = string.Format("{0, -10}  {1, -10}  {2, -10}", sevenmks, sevenmks, sixmks);
+                Console.WriteLine(border);//create border for each label.
+
+                for (int i = 1; i <= n; i++)//runs user input through a for loop that includes calculations.
                 {
                     int squared = i * i;
                     int cubed = i * i * i;
 
-                    string result = string.Format("{0, 10}  {1, 10}  {2, 10}",
-                        i,
-                        squared,
-                        cubed);
+                    string result = string.Format("{0, -10}  {1, -10}  {2, -10}", i, squared, cubed);//result converted to string to utilize string.Format.
 
                     Console.WriteLine(result);
                 }
 
                 Console.WriteLine("Would you like to continue? (y/n)");
-                input = Console.ReadLine();
+                input = Console.ReadLine().ToLower();
             }
         }
     }
